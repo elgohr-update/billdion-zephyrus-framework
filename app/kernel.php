213ee4fb9232298196042ce48adc26271c6a52ab
David Tucker <davidt2003@msn.com>
2017-01-04 17:13:11 -0500

    Added comments

--- app/kernel.php
+++ app/kernel.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !!!!! BOOTSTRAP FILE AUTOMATICALLY LOADED                              !!!!!
+ * !!!!! Make sure to properly link the framework.                        !!!!!
+ * !!!!!                                                                  !!!!!
+ * !!!!! SHOULD NOT BE MODIFIED                                           !!!!!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ */
 define('ROOT_DIR', __DIR__ . '/..');
 require ROOT_DIR . '/vendor/autoload.php';
 

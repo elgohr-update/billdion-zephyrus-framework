ca853940a19f65b819b14ff0dcf74e1e8953b789
David Tucker <davidt2003@msn.com>
2017-01-04 16:15:43 -0500

    Added example route with argument and added comments

--- app/routes/example.php
+++ app/routes/example.php
@@ -2,9 +2,22 @@
 
 /**
  * Simple example showcasing the basic usage of direct route definition instead
- * of using routable controllers. The routes directory can be removed if you
- * are only using controllers.
+ * of using routable controllers. Every php files under the /routes directory
+ * are automatically loaded. The $router variable is directly available in these
+ * files.
+ *
+ * The /routes directory can be removed completely if you are only using
+ * controllers.
  */
 $router->get("/example/static", function() {
     echo "it works !";
+});
+
+/**
+ * Route's arguments can be directly passed to the callback function of they can
+ * simply be accessed using Request::getParameter() method exactly as in a
+ * routable controller.
+ */
+$router->get("/example/static/{id}", function($id) {
+    echo "it works with id $id !";
 });

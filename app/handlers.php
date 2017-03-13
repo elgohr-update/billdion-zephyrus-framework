43caeb840492c6c77f61dafd6e7582979c7fd430
David Tucker <davidt2003@msn.com>
2017-03-13 08:39:06 -0400

    Empty handlers

--- app/handlers.php
+++ app/handlers.php
@@ -8,8 +8,6 @@
  * error handling as you see fit (redirection, disconnection, messages, ...).
  */
 use Zephyrus\Application\ErrorHandler;
-use Zephyrus\Application\Flash;
-use Zephyrus\Application\Session;
 use Zephyrus\Exceptions\RouteNotFoundException;
 use Zephyrus\Exceptions\UnauthorizedAccessException;
 use Zephyrus\Exceptions\InvalidCsrfException;
@@ -18,26 +16,25 @@ use Zephyrus\Exceptions\DatabaseException;
 $errorHandler = new ErrorHandler();
 
 $errorHandler->exception(function (Error $e) {
-    //TODO: handle any
+
 });
 
 $errorHandler->exception(function (Exception $e) {
-    //TODO: code to handle any type of Exceptions if not specify elsewhere
+
 });
 
 $errorHandler->exception(function (DatabaseException $e) {
-    //TODO: code to handle any type of database errors
+
 });
 
 $errorHandler->exception(function (RouteNotFoundException $e) {
-    //TODO: code to handle when the route does not exist
+
 });
 
 $errorHandler->exception(function(UnauthorizedAccessException $e) {
-    Session::getInstance()->restart();
-    Flash::error("Vous n'avez pas les droits requis pour accéder à la ressource spécifiée");
+
 });
 
 $errorHandler->exception(function (InvalidCsrfException $e) {
-    //TODO: code to handle when a CSRF token check fails
+
 });

e4c9176a40f62db4904f57629364080782ac44dd
David Tucker <davidt2003@msn.com>
2017-09-04 16:49:24 -0400

    Added abstract Broker basic class

---
+++ app/Models/Brokers/Broker.php
@@ -0,0 +1,49 @@
+<?php namespace Models\Brokers;
+
+use Zephyrus\Database\Database;
+
+abstract class Broker extends \Zephyrus\Database\Broker
+{
+    abstract protected function load(array $row);
+
+    public function __construct(?Database $database = null)
+    {
+        parent::__construct($database);
+        /*$this->applyConnectionVariables();*/
+    }
+
+    public function select($query, $parameters = [], $allowedTags = "")
+    {
+        return parent::selectUnique($query, $parameters, $allowedTags);
+    }
+
+    public function selectUnique($query, $parameters = [], $allowedTags = "")
+    {
+        $result = parent::selectUnique($query, $parameters, $allowedTags);
+        return (!$result) ? null : $this->load($result);
+    }
+
+    public function selectAll($query, $parameters = [], $allowedTags = "")
+    {
+        $results = [];
+        foreach (parent::selectAll($query, $parameters, $allowedTags) as $row) {
+            $results[] = $this->load($row);
+        }
+        return $results;
+    }
+
+    /**
+     * Sample code if you want to automatically send php data to the database
+     * environment which could be used inside a trigger for example. If this
+     * has no purpose in your project, you should remove this method.
+     */
+    /*private function applyConnectionVariables()
+    {
+        $userId = Session::getInstance()->read('id');
+        try {
+            parent::query("SET @user_id = ?", [$userId]);
+        } catch (\Exception $e) {
+
+        }
+    }*/
+}

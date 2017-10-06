b85f9393c85727b8caa01b32e34fc568850fcdd9
David Tucker <davidt2003@msn.com>
2017-10-06 08:53:29 -0400

    Added json example

--- app/Controllers/ExampleController.php
+++ app/Controllers/ExampleController.php
@@ -12,6 +12,7 @@ class ExampleController extends Controller
     public function initializeRoutes()
     {
         $this->get("/", "index");
+        $this->get("/items", "jsonTest");
     }
 
     /**
@@ -19,6 +20,41 @@ class ExampleController extends Controller
      */
     public function index()
     {
-        $this->render('example', ["currentDate" => date('Y-m-d')]);
+        return $this->render('example', ["currentDate" => date('Y-m-d')]);
+    }
+
+    /**
+     * Example route rendering json entities.
+     */
+    public function jsonTest()
+    {
+        $items = $this->buildItems();
+        return $this->json($items);
+    }
+
+    /**
+     * @return Item[]
+     */
+    private function buildItems(): array
+    {
+        $items = [];
+        $item = new Item();
+        $item->setId(1);
+        $item->setName("Batarang");
+        $item->setPrice(10.50);
+        $items[] = $item;
+
+        $item = new Item();
+        $item->setId(2);
+        $item->setName("Captain America Shield");
+        $item->setPrice(400);
+        $items[] = $item;
+
+        $item = new Item();
+        $item->setId(3);
+        $item->setName("Thor Hammer");
+        $item->setPrice(700);
+        $items[] = $item;
+        return $items;
     }
 }

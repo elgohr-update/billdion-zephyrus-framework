0ef070149d088f73479e15a2713a2838671ae5c2
David Tucker <davidt2003@msn.com>
2018-01-23 10:37:55 -0500

    Removed unused route subfolder

--- public/index.php
+++ public/index.php
@@ -1,11 +1,4 @@
 <?php
 
-use Zephyrus\Network\RequestFactory;
-use Zephyrus\Application\Bootstrap;
-
-foreach (recursiveGlob('../app/routes/*.php') as $file) {
-    include($file);
-}
-
-Bootstrap::initializeRoutableControllers($router);
-$router->run(RequestFactory::read());
+Zephyrus\Application\Bootstrap::initializeRoutableControllers($router);
+$router->run(Zephyrus\Network\RequestFactory::read());

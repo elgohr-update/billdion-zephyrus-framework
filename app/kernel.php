2503cb2c0bfb6a7f642c636b5b6a96d0db77db78
David Tucker <davidt2003@msn.com>
2020-03-15 19:22:08 -0400

    Added error handling to kernel file

--- app/kernel.php
+++ app/kernel.php
@@ -3,6 +3,9 @@
 // region Composer autoloading and Zephyrus instance
 // This part is essential for the correct inclusion of the Framework on part
 // with Composer dependency manager. Do not modify.
+use Zephyrus\Application\ErrorHandler;
+use Zephyrus\Exceptions\DatabaseException;
+use Zephyrus\Exceptions\RouteNotFoundException;
 use Zephyrus\Network\Router;
 use Zephyrus\Application\Bootstrap;
 use Zephyrus\Application\Session;
@@ -38,3 +41,49 @@ try {
     die($e->getMessage());
 }
 // endregion
+
+/**
+ * Defines how to handle errors and exceptions which reached the main
+ * thread (that nobody trapped). These are usage example and should be
+ * altered to reflect serious application usage. The ErrorHandler class
+ * allows to handle any specific exception as you see fit.
+ *
+ * Note that using the ErrorHandler changes the way PHP will handle
+ * errors at its core if you use notice(), warning() or error().
+ */
+function setupErrorHandling()
+{
+    $errorHandler = ErrorHandler::getInstance();
+
+    /**
+     * Handles basically every exceptions that were not caught.
+     */
+    $errorHandler->exception(function (Exception $e) {
+    });
+
+    /**
+     * Handles specific case when a database exception occurred. Depends on
+     * the need of each application. Some may want to specifically handle
+     * this case or catch them in the global Exception. In fact, it is
+     * possible to handle every exception specifically if needed.
+     */
+    $errorHandler->exception(function (DatabaseException $e) {
+    });
+
+    /**
+     * Handles when a user tries to access a route that doesn't exists. In
+     * this example, it simply returns a 404 header. You could implement a
+     * custom page to display a significant error, do a flash message and
+     * redirect, you could also log the attempt, etc. The exception
+     * contains the requested URL and http method.
+     */
+    $errorHandler->exception(function (RouteNotFoundException $e) {
+        $instance->abortNotFound();
+    });
+
+    // Its recommended to catch these exceptions in security middleware.
+    //$errorHandler->exception(function(UnauthorizedAccessException $e) {
+    //});
+    //$errorHandler->exception(function (InvalidCsrfException $e) {
+    //});
+}

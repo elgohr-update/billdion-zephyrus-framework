8c2cfbe4a4c24f38e195e6d3b2e157652c4baed9
David Tucker <davidt2003@msn.com>
2020-03-15 15:44:42 -0400

    Added router exception catch

--- public/index.php
+++ public/index.php
@@ -1,4 +1,19 @@
 <?php
 
-Zephyrus\Application\Bootstrap::initializeRoutableControllers($router);
-$router->run(Zephyrus\Network\RequestFactory::read());
+use Zephyrus\Application\Bootstrap;
+use Zephyrus\Exceptions\RouteMethodUnsupportedException;
+use Zephyrus\Exceptions\RouteNotAcceptedException;
+use Zephyrus\Exceptions\RouteNotFoundException;
+use Zephyrus\Network\ResponseFactory;
+
+Bootstrap::initializeRoutableControllers($router);
+
+try {
+    $router->run(Zephyrus\Network\RequestFactory::read());
+} catch (RouteMethodUnsupportedException $e) {
+    ResponseFactory::getInstance()->buildAbortMethodNotAllowed()->send();
+} catch (RouteNotAcceptedException $e) {
+    ResponseFactory::getInstance()->buildAbortNotAcceptable()->send();
+} catch (RouteNotFoundException $e) {
+    ResponseFactory::getInstance()->buildAbortNotFound()->send();
+}

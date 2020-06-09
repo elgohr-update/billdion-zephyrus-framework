98eaf21e4ce25f0888f753ee3183b995200c70af
dadajuice <davidt2003@msn.com>
2020-06-09 15:36:53 -0400

    Refactor for core version inclusion and beginning of setup

--- app/Controllers/SetupController.php
+++ app/Controllers/SetupController.php
@@ -2,21 +2,47 @@
 
 use Zephyrus\Exceptions\HttpRequesterException;
 use Zephyrus\Network\HttpRequester;
+use Zephyrus\Network\Response;
 
 class SetupController extends Controller
 {
     public function initializeRoutes()
     {
         $this->get("/", "index");
+        $this->get("/setup", "setup");
     }
 
     public function index()
     {
-        return $this->render('setup/landing', [
-            'zephyrus_version' => $this->latestRemoteVersion()
-        ]);
+        return $this->render('setup/landing');
     }
 
+    public function setup()
+    {
+        return $this->render('setup/start');
+    }
+
+    /**
+     * For this controller, all route rendering should include the latest Zephyrus core version in its parameters.
+     *
+     * @param string $page
+     * @param array $args
+     * @return Response
+     */
+    protected function render($page, $args = []): Response
+    {
+        return parent::render($page, array_merge($args, [
+            'system_date' => date(FORMAT_DATE_TIME),
+            'zephyrus_version' => $this->latestRemoteVersion()
+        ]));
+    }
+
+    /**
+     * Fetches the latest released core version on the official github repository.
+     *
+     * @param string $repository
+     * @return string
+     */
     private function latestRemoteVersion($repository = "dadajuice/zephyrus"): string
     {
         try {
